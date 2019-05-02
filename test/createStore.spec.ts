02291796203d0500e6c4fe9e22d2ed2a08406af9
Lee Tae Woo <lee111107@gmail.com>
2019-05-02 15:35:40 +0900

    Update test, package.json

--- test/createStore.spec.ts
+++ test/createStore.spec.ts
@@ -6,14 +6,14 @@ describe('createStore', () => {
 	let store: ReturnType<typeof createStore>;
 
 	before('create store', () => {
-		store = createStore('./test/store/storetest0.json');
+		store = createStore('./test/a/b/c/store/storetest0.json');
 	});
 
 	it('initialize', async () => {
 		await store.initialize();
 
 		const res = await fs.promises.readFile(
-			'./test/store/storetest0.json',
+			'./test/a/b/c/store/storetest0.json',
 			'utf8',
 		);
 
@@ -30,7 +30,10 @@ describe('createStore', () => {
 	});
 
 	after('end test, remove test file', async () => {
-		await fs.promises.unlink('./test/store/storetest0.json');
-		await fs.promises.rmdir('./test/store');
+		await fs.promises.unlink('./test/a/b/c/store/storetest0.json');
+		await fs.promises.rmdir('./test/a/b/c/store');
+		await fs.promises.rmdir('./test/a/b/c');
+		await fs.promises.rmdir('./test/a/b/');
+		await fs.promises.rmdir('./test/a/');
 	});
 });
