efc56b189738fe0efcb16c86d6b34a0d69fcd729
Lee Tae Woo <syr@meu.works>
2019-06-25 07:07:00 +0900

    add Store type

--- src/index.ts
+++ src/index.ts
@@ -4,41 +4,51 @@ import { stringifyJSON, parseJSON } from './lib/json';
 
 export { readFile, writeFile, pathExists, stringifyJSON, parseJSON };
 
+interface IStore<T> {
+    initialize: (init?: T) => void;
+    read: () => Promise<T>;
+    write: (data: T) => Promise<boolean>;
+}
+
+export type Store<T> = IStore<T>;
+
 const initialize = <T = any>(filePath: string) => async (init?: T) => {
-	if (!(await pathExists(filePath))) {
-		await mkdirp(path.dirname(filePath));
-		await write(filePath)(init || {});
-	}
+    if (!(await pathExists(filePath))) {
+        await mkdirp(path.dirname(filePath));
+        await write(filePath)(init || {});
+    }
 };
 
 const read = <T = any>(filePath: string) => async (): Promise<T> => {
-	const data = await readFile(filePath, 'utf8');
+    const data = await readFile(filePath, 'utf8');
 
-	const res = await parseJSON<T>(data);
+    const res = await parseJSON<T>(data);
 
-	return res;
+    return res;
 };
 
 const write = <T = any>(filePath: string) => async (
-	data: T,
+    data: T,
 ): Promise<boolean> => {
-	const stringified = await stringifyJSON(data);
+    const stringified = await stringifyJSON(data);
 
-	const res = writeFile(filePath, stringified, 'utf8');
+    const res = writeFile(filePath, stringified, 'utf8');
 
-	return res;
+    return res;
 };
 
-export const createStore = <T extends object = any>(filePath: string) => {
-	const rp = path.resolve(filePath);
-	return {
-		/**
+export const createStore = <T extends object = any>(
+    filePath: string,
+): Store<T> => {
+    const rp = path.resolve(filePath);
+    return {
+        /**
          * if not exists store file, initialize store with init value
          *
          * @param {object} [init={}] - some json
          */
-		initialize: initialize<T>(rp),
-		read: read<T>(rp),
-		write: write<T>(rp),
-	};
+        initialize: initialize<T>(rp),
+        read: read<T>(rp),
+        write: write<T>(rp),
+    };
 };
